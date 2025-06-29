b8d39c37a9926db71594cb45dba8f88114f053bb
Vincenzo Sicurella <sicurella12@gmail.com>
2025-06-28 23:06:39 -0400

    fix capitalization, env contexts

--- Source/LumatoneMenu.cpp
+++ Source/LumatoneMenu.cpp
@@ -43,7 +43,7 @@ namespace Lumatone {
 
 #if ! JUCE_MAC
 			menu.addSeparator();
-			menu.addCommandItem(theManager, juce::StandardApplicationCommandIds::quit);
+			menu.addCommandItem(theManager, juce::StandardApplicationCommandIDs::quit);
 #endif
 		}
 
